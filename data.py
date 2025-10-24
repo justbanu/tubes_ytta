2ff61c601bc72f3a2c4a65440b9193d85428a8f2
justbanu <banuagilschool@gmail.com>
2025-10-24 14:10:23 +0800

    Add files via upload

---
+++ data.py
@@ -0,0 +1,4 @@
+barang_array = ["le minerale", "cacacola", "tango", "mentos", "milo kotak"]
+harga_array = [5000, 8000, 10000, 7000, 9000]
+stok_array = [10, 10, 10, 10, 10]
+kategori_array = ["minuman", "minuman", "makanan", "makanan", "minuman"]
